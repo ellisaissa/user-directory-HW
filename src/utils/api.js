a5fe311af3272099fdf9c4d0e6d94ec815d963ee
ellisa <ellisaissa@gmail.com>
2021-07-07 19:12:33 -0400

    files
    added more folders and files

---
+++ src/utils/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const directoryUrl =
+  "https://randomuser.me/api/?nat=au,ca,gb,us,nz&results=100";
+
+export default {
+  fetchRandomEmployees: function () {
+    return axios.get(directoryUrl);
+  },
+};
